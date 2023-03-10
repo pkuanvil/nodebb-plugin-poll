pkuanvil/nodebb-plugin-poll

commit 84ff029311d965a1262ef9a0992b7d21223597e2

refactor: upgrade to recent standards like ES2017 await/async

lib/poll.js
@@ -1,298 +1,213 @@
 'use strict';
 
-const async = require('async');
-
-const NodeBB = require('./nodebb');
+const db = require.main.require('./src/database');
+const utils = require.main.require('./src/utils');
 
 const Vote = require('./vote');
 const Scheduler = require('./scheduler');
 
-(function (Poll) {
-	const POLL_DATA_VERSION = 2;
+const Poll = module.exports;
 
-	Poll.add = function (pollData, postData, callback) {
-		NodeBB.db.incrObjectField('global', 'nextPollId', (err, pollId) => {
-			if (err) {
-				return callback(err);
-			}
+const POLL_DATA_VERSION = 2;
 
-			const poll = {
-				pollId: pollId,
-				uid: postData.editor || postData.uid,
-				tid: postData.tid,
-				pid: postData.pid,
-				deleted: 0,
-				ended: 0,
-				timestamp: postData.edited || postData.timestamp,
-				version: POLL_DATA_VERSION,
-			};
+Poll.add = async function (pollData, postData) {
+	const pollId = await db.incrObjectField('global', 'nextPollId');
 
-			pollData.options = pollData.options.map((val, i) => ({
-				id: i,
-				title: val,
-			}));
-
-			// async this bitch up
-			async.parallel([
-				async.apply(async.each, pollData.options, (option, next) => {
-					async.series([
-						async.apply(NodeBB.db.setObject, `poll:${pollId}:options:${option.id}`, option),
-						async.apply(NodeBB.db.setAdd, `poll:${pollId}:options`, option.id),
-					], next);
-				}),
-				async.apply(NodeBB.db.setObject, `poll:${pollId}`, poll),
-				async.apply(NodeBB.db.setObject, `poll:${pollId}:settings`, pollData.settings),
-				async.apply(NodeBB.db.listAppend, 'polls', pollId),
-				async.apply(NodeBB.db.setObjectField, `topic:${poll.tid}`, 'pollId', pollId),
-			], (err) => {
-				if (err) {
-					return callback(err);
-				}
-
-				// Check if this poll is scheduled to end
-				if (parseInt(pollData.settings.end, 10) > 0) {
-					Poll.schedule(pollId);
-				}
-
-				return callback(null, pollId);
-			});
-		});
+	const poll = {
+		pollId: pollId,
+		uid: postData.editor || postData.uid,
+		tid: postData.tid,
+		pid: postData.pid,
+		deleted: 0,
+		ended: 0,
+		timestamp: postData.edited || postData.timestamp,
+		version: POLL_DATA_VERSION,
 	};
 
-	Poll.get = function (pollId, uid, withVotes, callback) {
-		async.parallel({
-			info: function (next) {
-				Poll.getInfo(pollId, next);
-			},
-			options: function (next) {
-				Poll.getOptions(pollId, withVotes, next);
-			},
-			settings: function (next) {
-				Poll.getSettings(pollId, next);
-			},
-			hasVoted: function (next) {
-				if (uid) {
-					Vote.hasUidVoted(uid, pollId, next);
-				} else {
-					next(null, false);
-				}
-			},
-			vote: function (next) {
-				if (uid) {
-					Vote.getUidVote(uid, pollId, next);
-				} else {
-					next(null, null);
-				}
-			},
-		}, (err, pollData) => {
-			if (err) {
-				return callback(err);
-			}
-			pollData.options.forEach((option) => {
-				const percentage = ((option.voteCount / pollData.info.voteCount) * 100).toFixed(0);
-				option.percentage = isNaN(percentage) ? 0 : percentage;
-			});
-			pollData.settings.disallowVoteUpdate = parseInt(pollData.settings.disallowVoteUpdate, 10);
-			callback(null, pollData);
-		});
-	};
+	pollData.options = pollData.options.map((val, i) => ({
+		id: i,
+		title: val,
+	}));
 
-	Poll.getPollIdByTid = function (tid, callback) {
-		NodeBB.db.getObjectField(`topic:${tid}`, 'pollId', callback);
-	};
+	// async this bitch up
+	await Promise.all([
+		...pollData.options.map(async (option) => {
+			await db.setObject(`poll:${pollId}:options:${option.id}`, option);
+			await db.setAdd(`poll:${pollId}:options`, option.id);
+		}),
+		db.setObject(`poll:${pollId}`, poll),
+		db.setObject(`poll:${pollId}:settings`, pollData.settings),
+		db.listAppend('polls', pollId),
+		db.setObjectField(`topic:${poll.tid}`, 'pollId', pollId),
+	]);
 
-	Poll.getPollIdByPid = function (pid, callback) {
-		NodeBB.db.getObjectField(`post:${pid}`, 'pollId', callback);
-	};
+	// Check if this poll is scheduled to end
+	if (parseInt(pollData.settings.end, 10) > 0) {
+		await Poll.schedule(pollId);
+	}
 
-	Poll.getInfo = function (pollId, callback) {
-		async.parallel({
-			poll: function (next) {
-				NodeBB.db.getObject(`poll:${pollId}`, next);
-			},
-			voteCount: function (next) {
-				Poll.getVoteCount(pollId, next);
-			},
-		}, (err, results) => {
-			if (err) {
-				return callback(err);
-			}
-			results.poll.voteCount = parseInt(results.voteCount, 10) || 0;
-			callback(null, results.poll);
-		});
-	};
+	return pollId;
+};
 
-	Poll.getOptions = function (pollId, withVotes, callback) {
-		if (typeof withVotes === 'function') {
-			callback = withVotes;
-			withVotes = false;
-		}
+Poll.get = async function (pollId, uid, withVotes) {
+	const pollData = await utils.promiseParallel({
+		info: Poll.getInfo(pollId),
+		options: Poll.getOptions(pollId, withVotes),
+		settings: Poll.getSettings(pollId),
+		hasVoted: uid ? Vote.hasUidVoted(uid, pollId) : Promise.resolve(false),
+		vote: uid ? Vote.getUidVote(uid, pollId) : Promise.resolve(null),
+	});
+	pollData.options.forEach((option) => {
+		const percentage = ((option.voteCount / pollData.info.voteCount) * 100).toFixed(0);
+		option.percentage = isNaN(percentage) ? 0 : percentage;
+	});
+	pollData.settings.disallowVoteUpdate = parseInt(pollData.settings.disallowVoteUpdate, 10);
+	return pollData;
+};
 
-		NodeBB.db.getSetMembers(`poll:${pollId}:options`, (err, options) => {
-			if (err) {
-				return callback(err);
-			}
-			async.map(options, (option, next) => {
-				Poll.getOption(pollId, option, withVotes, next);
-			}, callback);
-		});
-	};
+Poll.getPollIdByTid = async function (tid) {
+	return await db.getObjectField(`topic:${tid}`, 'pollId');
+};
 
-	Poll.getOption = function (pollId, option, withVotes, callback) {
-		async.parallel({
-			option: function (next) {
-				NodeBB.db.getObject(`poll:${pollId}:options:${option}`, next);
-			},
-			votes: function (next) {
-				if (withVotes) {
-					NodeBB.db.getSortedSetRange(`poll:${pollId}:options:${option}:votes`, 0, -1, next);
-				} else {
-					next();
-				}
-			},
-			voteCount: function (next) {
-				Poll.getOptionVoteCount(pollId, option, next);
-			},
-		}, (err, results) => {
-			if (err) {
-				return callback(err);
-			}
-			if (results.votes) {
-				results.option.votes = results.votes;
-			}
-			results.option.voteCount = parseInt(results.voteCount, 10) || 0;
+Poll.getPollIdByPid = async function (pid) {
+	return await db.getObjectField(`post:${pid}`, 'pollId');
+};
 
-			callback(null, results.option);
-		});
-	};
+Poll.getInfo = async function (pollId) {
+	const results = await utils.promiseParallel({
+		poll: db.getObject(`poll:${pollId}`),
+		voteCount: Poll.getVoteCount(pollId),
+	});
+	results.poll.voteCount = parseInt(results.voteCount, 10) || 0;
+	return results.poll;
+};
 
-	Poll.getVotersCount = function (pollId, callback) {
-		NodeBB.db.sortedSetCard(`poll:${pollId}:voters`, callback);
-	};
+Poll.getOptions = async function (pollId, withVotes) {
+	const options = await db.getSetMembers(`poll:${pollId}:options`);
+	return await Promise.all(options.map(option => Poll.getOption(pollId, option, withVotes)));
+};
 
-	Poll.getVoteCount = function (pollId, callback) {
-		NodeBB.db.getSetMembers(`poll:${pollId}:options`, (err, options) => {
-			if (err) {
-				console.error(err);
-			}
+Poll.getOption = async function (pollId, option, withVotes) {
+	const results = await utils.promiseParallel({
+		option: db.getObject(`poll:${pollId}:options:${option}`),
+		votes: withVotes ?
+			db.getSortedSetRange(`poll:${pollId}:options:${option}:votes`, 0, -1) :
+			Promise.resolve(undefined),
+		voteCount: Poll.getOptionVoteCount(pollId, option),
+	});
+	if (results.votes) {
+		results.option.votes = results.votes;
+	}
+	results.option.voteCount = parseInt(results.voteCount, 10) || 0;
 
-			async.map(options, (option, next) => {
-				Poll.getOptionVoteCount(pollId, option, next);
-			}, (err, results) => {
-				if (err) {
-					return callback(err);
-				}
-				callback(null, results.reduce((a, b) => a + b, 0));
-			});
-		});
-	};
+	return results.option;
+};
 
-	Poll.getOptionVoteCount = function (pollId, option, callback) {
-		NodeBB.db.sortedSetCard(`poll:${pollId}:options:${option}:votes`, callback);
-	};
+Poll.getVotersCount = async function (pollId) {
+	return await db.sortedSetCard(`poll:${pollId}:voters`);
+};
 
-	Poll.hasOption = function (pollId, option, callback) {
-		NodeBB.db.isSetMember(`poll:${pollId}:options`, option, callback);
-	};
+Poll.getVoteCount = async function (pollId) {
+	const options = await db.getSetMembers(`poll:${pollId}:options`);
+	const results = await Promise.all(options.map(option => Poll.getOptionVoteCount(pollId, option)));
+	return results.reduce((a, b) => a + b);
+};
 
-	Poll.hasOptions = function (pollId, options, callback) {
-		NodeBB.db.isSetMembers(`poll:${pollId}:options`, options, callback);
-	};
+Poll.getOptionVoteCount = async function (pollId, option) {
+	return await db.sortedSetCard(`poll:${pollId}:options:${option}:votes`);
+};
 
-	Poll.getSettings = function (pollId, callback) {
-		NodeBB.db.getObject(`poll:${pollId}:settings`, callback);
-	};
+Poll.hasOption = async function (pollId, option) {
+	return await db.isSetMember(`poll:${pollId}:options`, option);
+};
 
-	Poll.isDeleted = function (pollId, callback) {
-		Poll.getField(pollId, 'deleted', (err, result) => {
-			callback(err, parseInt(result, 10) === 1);
-		});
-	};
+Poll.hasOptions = async function (pollId, options) {
+	return await db.isSetMembers(`poll:${pollId}:options`, options);
+};
 
-	Poll.delete = function (pollId) {
-		Poll.setField(pollId, 'deleted', 1);
-	};
+Poll.getSettings = async function (pollId) {
+	return await db.getObject(`poll:${pollId}:settings`);
+};
 
-	Poll.restore = function (pollId) {
-		Poll.setField(pollId, 'edited', 0);
-		Poll.setField(pollId, 'deleted', 0);
-	};
+Poll.isDeleted = async function (pollId) {
+	const result = await Poll.getField(pollId, 'deleted');
+	return parseInt(result, 10) === 1;
+};
 
-	Poll.schedule = function (pollId) {
-		NodeBB.db.setAdd('polls:scheduled', pollId);
-		Scheduler.add(pollId);
-	};
+Poll.delete = async function (pollId) {
+	return await Poll.setField(pollId, 'deleted', 1);
+};
 
-	Poll.getScheduled = function (callback) {
-		NodeBB.db.getSetMembers('polls:scheduled', callback);
-	};
+Poll.restore = async function (pollId) {
+	return await Poll.setFieldBulk(pollId, {
+		edited: 0,
+		deleted: 0,
+	});
+};
 
-	Poll.hasEnded = function (pollId, callback) {
-		Poll.getField(pollId, 'ended', (err, result) => {
-			callback(err, parseInt(result, 10) === 1);
-		});
-	};
+Poll.schedule = async function (pollId) {
+	await db.setAdd('polls:scheduled', pollId);
+	await Scheduler.add(pollId);
+};
 
-	Poll.doesDisallowVoteUpdate = function (pollId, callback) {
-		Poll.getSettingsField(pollId, 'disallowVoteUpdate', (err, result) => {
-			callback(err, parseInt(result, 10) === 1);
-		});
-	};
+Poll.getScheduled = async function () {
+	return await db.getSetMembers('polls:scheduled');
+};
 
-	Poll.doesAllowVoteUpdate = function (pollId, callback) {
-		Poll.getSettingsField(pollId, 'disallowVoteUpdate', (err, result) => {
-			callback(err, parseInt(result, 10) !== 1);
-		});
-	};
+Poll.hasEnded = async function (pollId) {
+	const result = await Poll.getField(pollId, 'ended');
+	return parseInt(result, 10) === 1;
+};
 
-	Poll.end = function (pollId) {
-		NodeBB.db.setRemove('polls:scheduled', pollId);
-		Poll.setField(pollId, 'ended', 1);
-	};
+Poll.doesDisallowVoteUpdate = async function (pollId) {
+	const result = await Poll.getSettingsField(pollId, 'disallowVoteUpdate');
+	return parseInt(result, 10) === 1;
+};
 
-	Poll.changePid = function (pollId, pid, callback) {
-		async.parallel([
-			function (next) {
-				Poll.setField(pollId, 'pid', pid, next);
-			},
-			function (next) {
-				NodeBB.db.setObjectField(`post:${pid}`, 'pollId', pollId, next);
-			},
-		], callback);
-	};
+Poll.doesAllowVoteUpdate = async function (pollId) {
+	const result = await Poll.getSettingsField(pollId, 'disallowVoteUpdate');
+	return parseInt(result, 10) !== 1;
+};
 
-	Poll.changeTid = function (pollId, tid, callback) {
-		async.parallel([
-			function (next) {
-				Poll.setField(pollId, 'tid', tid, next);
-			},
-			function (next) {
-				NodeBB.db.setObjectField(`topic:${tid}`, 'pollId', pollId, next);
-			},
-		], callback);
-	};
+Poll.end = async function (pollId) {
+	await db.setRemove('polls:scheduled', pollId);
+	await Poll.setField(pollId, 'ended', 1);
+};
 
-	Poll.setField = function (pollId, field, value, callback) {
-		NodeBB.db.setObjectField(`poll:${pollId}`, field, value, callback);
-	};
+Poll.changePid = async function (pollId, pid) {
+	return await Promise.all([
+		Poll.setField(pollId, 'pid', pid),
+		db.setObjectField(`post:${pid}`, 'pollId', pollId),
+	]);
+};
 
-	Poll.setFields = function (pollId, fields, values, callback) {
-		NodeBB.db.setObjectFields(`poll:${pollId}`, fields, values, callback);
-	};
+Poll.changeTid = async function (pollId, tid) {
+	return await Promise.all([
+		Poll.setField(pollId, 'tid', tid),
+		db.setObjectField(`topic:${tid}`, 'pollId', pollId),
+	]);
+};
 
-	Poll.getField = function (pollId, field, callback) {
-		NodeBB.db.getObjectField(`poll:${pollId}`, field, callback);
-	};
+Poll.setField = async function (pollId, field, value) {
+	return db.setObjectField(`poll:${pollId}`, field, value);
+};
 
-	Poll.getFields = function (pollId, fields, callback) {
-		NodeBB.db.getObjectFields(`poll:${pollId}`, fields, callback);
-	};
+Poll.setFieldBulk = async function (pollId, data) {
+	return await db.setObjectBulk([`poll:${pollId}`, data]);
+};
 
-	Poll.getSettingsField = function (pollId, field, callback) {
-		NodeBB.db.getObjectField(`poll:${pollId}:settings`, field, callback);
-	};
+Poll.getField = async function (pollId, field) {
+	return await db.getObjectField(`poll:${pollId}`, field);
+};
 
-	Poll.setSettingsField = function (pollId, field, value, callback) {
-		NodeBB.db.setObjectField(`poll:${pollId}:settings`, field, callback);
-	};
-}(exports));
+Poll.getFields = async function (pollId, fields) {
+	return await db.getObjectFields(`poll:${pollId}`, fields);
+};
+
+Poll.getSettingsField = async function (pollId, field) {
+	return await db.getObjectField(`poll:${pollId}:settings`, field);
+};
+
+Poll.setSettingsField = async function (pollId, field, value) {
+	return await db.setObjectField(`poll:${pollId}:settings`, field, value);
+};

lib/config.js
@@ -1,48 +1,51 @@
 'use strict';
 
-const NodeBB = require('./nodebb');
+const util = require.main.require('node:util');
+const Settings = require.main.require('./src/settings');
 
 const packageInfo = require('../package.json');
 const pluginInfo = require('../plugin.json');
 
 const pluginId = pluginInfo.id.replace('nodebb-plugin-', '');
 
-(function (Config) {
-	Config.plugin = {
-		name: pluginInfo.name,
-		id: pluginId,
-		version: packageInfo.version,
-		description: packageInfo.description,
-		icon: 'fa-bar-chart-o',
-	};
+const Config = module.exports;
 
-	Config.defaults = {
-		toggles: {
-			allowAnon: false,
-		},
-		limits: {
-			maxOptions: 10,
-		},
-		defaults: {
-			title: 'Poll',
-			maxvotes: 1,
-			disallowVoteUpdate: 0,
-			end: 0,
-		},
-	};
+Config.plugin = {
+	name: pluginInfo.name,
+	id: pluginId,
+	version: packageInfo.version,
+	description: packageInfo.description,
+	icon: 'fa-bar-chart-o',
+};
 
-	Config.settings = {};
+Config.defaults = {
+	toggles: {
+		allowAnon: false,
+	},
+	limits: {
+		maxOptions: 10,
+	},
+	defaults: {
+		title: 'Poll',
+		maxvotes: 1,
+		disallowVoteUpdate: 0,
+		end: 0,
+	},
+};
 
-	Config.init = function (callback) {
-		Config.settings = new NodeBB.Settings(Config.plugin.id, Config.plugin.version, Config.defaults, callback);
-	};
+Config.settings = {};
 
-	Config.adminSockets = {
-		sync: function () {
-			Config.settings.sync();
-		},
-		getDefaults: function (socket, data, callback) {
-			callback(null, Config.settings.createDefaultWrapper());
-		},
-	};
-}(exports));
+function initCb(__, callback) {
+	Config.settings = new Settings(Config.plugin.id, Config.plugin.version, Config.defaults, callback);
+}
+
+Config.init = util.promisify(initCb);
+
+Config.adminSockets = {
+	sync: function () {
+		Config.settings.sync();
+	},
+	getDefaults: function (socket, data, callback) {
+		callback(null, Config.settings.createDefaultWrapper());
+	},
+};

lib/hooks.js
@@ -1,134 +1,109 @@
 'use strict';
 
-const util = require('util');
-const NodeBB = require('./nodebb');
+const Posts = require.main.require('./src/posts');
+const Topics = require.main.require('./src/topics');
+const Privileges = require.main.require('./src/privileges');
 
 const Config = require('./config');
 const Poll = require('./poll');
 const Serializer = require('./serializer');
 
+const Hooks = module.exports;
 
-(function (Hooks) {
-	Hooks.filter = {};
-	Hooks.action = {};
+Hooks.filter = {};
+Hooks.action = {};
 
-	Hooks.filter.parseRaw = function (raw, callback) {
-		callback(null, Serializer.removeMarkup(raw, '[Poll]'));
-	};
+Hooks.filter.parseRaw = async function (raw) {
+	return Serializer.removeMarkup(raw, '[Poll]');
+};
 
-	Hooks.filter.postCreate = async function (obj) {
-		if (Serializer.hasMarkup(obj.post.content) && obj.data.isMain) {
-			return await savePoll(obj);
-		}
+Hooks.filter.postCreate = async function (obj) {
+	if (Serializer.hasMarkup(obj.post.content) && obj.data.isMain) {
+		return await savePoll(obj);
+	}
+	return obj;
+};
+
+Hooks.filter.postEdit = async function (obj) {
+	const { tid, pollId } = await Posts.getPostFields(obj.data.pid, ['tid', 'pollId']);
+	if (pollId || !Serializer.hasMarkup(obj.post.content)) {
 		return obj;
-	};
+	}
 
-	Hooks.filter.postEdit = async function (obj) {
-		const { tid, pollId } = await NodeBB.Posts.getPostFields(obj.data.pid, ['tid', 'pollId']);
-		if (pollId || !Serializer.hasMarkup(obj.post.content)) {
-			return obj;
-		}
-
-		const result = await NodeBB.Topics.getTopicFields(tid, ['mainPid', 'cid']);
-		if (parseInt(result.mainPid, 10) !== parseInt(obj.data.pid, 10)) {
-			return obj;
-		}
-
-		await canCreate(result.cid, obj.post.editor);
-
-		const postData = await savePoll({
-			...obj.post,
-			uid: obj.data.uid,
-			pid: obj.data.pid,
-			tid: tid,
-		});
-		delete postData.uid;
-		delete postData.pid;
-		delete postData.tid;
-		obj.post = postData;
-
-		if (!postData.pollId) {
-			return obj;
-		}
-
-		// NodeBB only updates the edited, editor and content fields, so we add the pollId field manually.
-		await NodeBB.Posts.setPostField(obj.data.pid, 'pollId', postData.pollId);
+	const result = await Topics.getTopicFields(tid, ['mainPid', 'cid']);
+	if (parseInt(result.mainPid, 10) !== parseInt(obj.data.pid, 10)) {
 		return obj;
-	};
+	}
 
-	Hooks.filter.topicPost = async function (data) {
-		if (Serializer.hasMarkup(data.content)) {
-			await canCreate(data.cid, data.uid);
-			return data;
-		}
+	await canCreate(result.cid, obj.post.editor);
+
+	const postData = await savePoll({
+		...obj.post,
+		uid: obj.data.uid,
+		pid: obj.data.pid,
+		tid: tid,
+	});
+	delete postData.uid;
+	delete postData.pid;
+	delete postData.tid;
+	obj.post = postData;
+
+	if (!postData.pollId) {
+		return obj;
+	}
+
+	// NodeBB only updates the edited, editor and content fields, so we add the pollId field manually.
+	await Posts.setPostField(obj.data.pid, 'pollId', postData.pollId);
+	return obj;
+};
+
+Hooks.filter.topicPost = async function (data) {
+	if (Serializer.hasMarkup(data.content)) {
+		await canCreate(data.cid, data.uid);
 		return data;
-	};
-
-	Hooks.action.postDelete = function (data) {
-		Poll.getPollIdByPid(data.post.pid, (err, pollId) => {
-			if (err) {
-				console.error(err);
-			}
-			if (pollId) {
-				Poll.delete(pollId);
-			}
-		});
-	};
-
-	Hooks.action.postRestore = function (data) {
-		Poll.getPollIdByPid(data.post.pid, (err, pollId) => {
-			if (err) {
-				console.error(err);
-			}
-			if (pollId) {
-				Poll.restore(pollId);
-			}
-		});
-	};
-
-	Hooks.action.topicDelete = function (data) {
-		Poll.getPollIdByTid(data.topic.tid, (err, pollId) => {
-			if (err) {
-				console.error(err);
-			}
-			if (pollId) {
-				Poll.delete(pollId);
-			}
-		});
-	};
-
-	Hooks.action.topicRestore = function (data) {
-		Poll.getPollIdByTid(data.topic.tid, (err, pollId) => {
-			if (err) {
-				console.error(err);
-			}
-			if (pollId) {
-				Poll.restore(pollId);
-			}
-		});
-	};
-
-	async function canCreate(cid, uid) {
-		const can = await NodeBB.Privileges.categories.can('poll:create', cid, uid);
-		if (!can) {
-			throw new Error('[[poll:error.privilege.create]]');
-		}
 	}
+	return data;
+};
 
-	async function savePoll(obj) {
-		const postobj = obj.post ? obj.post : obj;
-		const pollData = Serializer.serialize(postobj.content, Config.settings.get());
+Hooks.action.postDelete = async function (data) {
+	const pollId = await Poll.getPollIdByPid(data.post.pid);
+	await Poll.delete(pollId);
+};
 
-		if (!pollData || !pollData.options.length) {
-			return obj;
-		}
+Hooks.action.postRestore = async function (data) {
+	const pollId = await Poll.getPollIdByPid(data.post.pid);
+	await Poll.restore(pollId);
+};
 
-		const addPoll = util.promisify(Poll.add);
-		const pollId = await addPoll(pollData, postobj);
+Hooks.action.topicDelete = async function (data) {
+	const pollId = await Poll.getPollIdByTid(data.topic.tid);
+	await Poll.delete(pollId);
+};
 
-		postobj.pollId = pollId;
-		postobj.content = Serializer.removeMarkup(postobj.content);
+Hooks.action.topicRestore = async function (data) {
+	const pollId = await Poll.getPollIdByTid(data.topic.tid);
+	await Poll.restore(pollId);
+};
 
+async function canCreate(cid, uid) {
+	const can = await Privileges.categories.can('poll:create', cid, uid);
+	if (!can) {
+		throw new Error('[[poll:error.privilege.create]]');
+	}
+}
+
+async function savePoll(obj) {
+	const postobj = obj.post ? obj.post : obj;
+	const pollData = Serializer.serialize(postobj.content, Config.settings.get());
+
+	if (!pollData || !pollData.options.length) {
 		return obj;
 	}
-}(exports));
+
+	const pollId = await Poll.add(pollData, postobj);
+
+	postobj.pollId = pollId;
+	postobj.content = Serializer.removeMarkup(postobj.content);
+
+	return obj;
+}

lib/nodebb.js
@@ -1,17 +1,0 @@
-'use strict';
-
-module.exports = {
-	db: require.main.require('./src/database'),
-	Settings: require.main.require('./src/settings'),
-	Meta: require.main.require('./src/meta'),
-	User: require.main.require('./src/user'),
-	Posts: require.main.require('./src/posts'),
-	Topics: require.main.require('./src/topics'),
-	Privileges: require.main.require('./src/privileges'),
-	PluginSockets: require.main.require('./src/socket.io/plugins'),
-	AdminSockets: require.main.require('./src/socket.io/admin').plugins,
-	SocketIndex: require.main.require('./src/socket.io/index'),
-	Translator: require.main.require('./src/translator'),
-	winston: require.main.require('winston'),
-	app: null,
-};

lib/scheduler.js
@@ -1,63 +1,50 @@
 'use strict';
 
-const cron = require('cron').CronJob;
-const NodeBB = require('./nodebb');
+const cron = require.main.require('cron').CronJob;
+const winston = require.main.require('winston');
 
 const Poll = require('./poll');
 
-(function (Scheduler) {
-	const jobs = {};
+const Scheduler = module.exports;
 
-	Scheduler.start = function () {
-		Poll.getScheduled((err, pollIds) => {
-			if (err) {
-				console.error(err);
-			}
-			pollIds.forEach((pollId) => {
-				Scheduler.add(pollId);
-			});
-		});
-	};
+const jobs = new Map();
 
-	Scheduler.add = function (pollId) {
-		if (Object.keys(jobs).indexOf(pollId.toString()) !== -1) {
-			return;
-		}
+Scheduler.start = async function () {
+	const PollIds = (await Poll.getScheduled()) || [];
+	await Promise.all(PollIds.map(pollId => Scheduler.add(pollId)));
+};
 
-		Poll.getSettings(pollId, (err, settings) => {
-			if (err) {
-				return NodeBB.winston.error(err);
-			}
+Scheduler.add = async function (pollId) {
+	if (jobs.has(pollId)) {
+		return;
+	}
 
-			if (!settings) {
-				return NodeBB.winston.error(`[nodebb-plugin-poll/scheduler] Poll ID ${pollId} has no settings!`);
-			}
+	const settings = await Poll.getSettings(pollId);
 
-			const now = Date.now();
-			const end = parseInt(settings.end, 10);
+	if (!settings) {
+		throw new Error(`[nodebb-plugin-poll/scheduler] Poll ID ${pollId} has no settings!`);
+	}
 
-			if (end < now) {
-				Scheduler.end(pollId);
-			} else {
-				const date = new Date(end);
-				NodeBB.winston.verbose(`[nodebb-plugin-poll/scheduler] Starting scheduler for poll with ID ${pollId} to end on ${date}`);
-				jobs[pollId] = new cron(date, (() => {
-					Scheduler.end(pollId);
-				}), null, true);
-			}
-		});
-	};
+	const now = Date.now();
+	const end = parseInt(settings.end, 10);
 
-	Scheduler.end = function (pollId) {
-		NodeBB.winston.verbose(`[nodebb-plugin-poll/scheduler] Ending poll with ID ${pollId}`);
+	if (end < now) {
+		await Scheduler.end(pollId);
+	} else {
+		const date = new Date(end);
+		winston.verbose(`[nodebb-plugin-poll/scheduler] Starting scheduler for poll with ID ${pollId} to end on ${date}`);
+		jobs.set(pollId, new cron(date, (() => Scheduler.end(pollId)), null, true));
+	}
+};
 
-		const index = Object.keys(jobs).indexOf(pollId.toString());
+Scheduler.end = async function (pollId) {
+	winston.verbose(`[nodebb-plugin-poll/scheduler] Ending poll with ID ${pollId}`);
 
-		if (index !== -1 && jobs[pollId] !== undefined) {
-			jobs[pollId].stop();
-			delete jobs[pollId];
-		}
+	const job = jobs.get(pollId);
+	if (job) {
+		job.stop();
+		jobs.delete(pollId);
+	}
 
-		Poll.end(pollId);
-	};
-}(exports));
+	await Poll.end(pollId);
+};

commit 4e733da83a5d1f77abd8bb309b65f7e83dd366a7

fix: setFields regression by commit 84ff0293

lib/poll.js
@@ -142,10 +142,7 @@ Poll.delete = async function (pollId) {
 };
 
 Poll.restore = async function (pollId) {
-	return await Poll.setFieldBulk(pollId, {
-		edited: 0,
-		deleted: 0,
-	});
+	return await Poll.setFields(pollId, ['edited', 'deleted'], [0, 0]);
 };
 
 Poll.schedule = async function (pollId) {
@@ -195,8 +192,12 @@ Poll.setField = async function (pollId, field, value) {
 	return db.setObjectField(`poll:${pollId}`, field, value);
 };
 
-Poll.setFieldBulk = async function (pollId, data) {
-	return await db.setObjectBulk([`poll:${pollId}`, data]);
+Poll.setFields = async function (pollId, fields, values) {
+	const data = {};
+	for (let i = 0; i < fields.length; i++) {
+		data[fields[i]] = values[i];
+	}
+	return await db.setObject(`poll:${pollId}`, data);
 };
 
 Poll.getField = async function (pollId, field) {